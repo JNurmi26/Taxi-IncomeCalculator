ad84d4b63ea8ae578b278c1501d3d8e3143ffdc9
Jani Nurmi <jani.nurmi@edu.turkuamk.fi>
2023-08-23 14:51:50 +0300

    project base

---
+++ js/incomecalc.js
@@ -0,0 +1,22 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+    const calculateButton = document.getElementById("calculateButton");
+    calculateButton.addEventListener("click", calculateIncome);
+});
+
+function calculateIncome() {
+    const netIncome = parseFloat(document.getElementById("netIncome").value);
+
+    if (isNaN(netIncome)) {
+        alert("Please enter a valid number for net income.");
+        return;
+    }
+
+    const grossMain = (netIncome * 0.35).toFixed(2);
+    const grossCalc1 = (grossMain * 0.07).toFixed(2);
+    const grossCalc2 = (grossMain * 0.0715).toFixed(2);
+    const grossCalc3 = (grossMain * 0.015).toFixed(2);
+    const grossIncome = (grossMain - grossCalc1 - grossCalc2 - grossCalc3).toFixed(2);
+
+    document.getElementById("grossIncome").textContent = grossIncome;
+}
