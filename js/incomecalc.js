6ff0a97ffbe9429a14b080c98ae20e406b6f2b7f
Jani Nurmi <jani.nurmi@edu.turkuamk.fi>
2023-09-12 14:36:51 +0300

    added descriptions and fixed the calculation

--- js/incomecalc.js
+++ js/incomecalc.js
@@ -12,11 +12,19 @@ function calculateIncome() {
         return;
     }
 
-    const netMain = (grossIncome * 0.35).toFixed(2);
-    const netCalc1 = (netMain * 0.07).toFixed(2);
-    const netCalc2 = (netMain * 0.0715).toFixed(2);
-    const netCalc3 = (netMain * 0.015).toFixed(2);
-    const netIncome = (netMain - netCalc1 - netCalc2 - netCalc3).toFixed(2);
+    //calculate the net income after 10% alv tax.
+    const netMain = (grossIncome / 1.1).toFixed(2);
+
+    //calculate the provision amount.
+    const netMain1 = (netMain * 0.35).toFixed(2);
+
+    //calculate taxes and expenses from provision.
+    const netCalc1 = (netMain1 * 0.07).toFixed(2);
+    const netCalc2 = (netMain1 * 0.0715).toFixed(2);
+    const netCalc3 = (netMain1 * 0.015).toFixed(2);
+
+    //substract all of the tax and expense amounts from the income after 10% alv tax to get net income.
+    const netIncome = (netMain1 - netCalc1 - netCalc2 - netCalc3).toFixed(2);
 
     document.getElementById("netIncome").textContent = netIncome;
 }
